c810c6391dd75f73e9f054c968b01aff2670ee4f
SIM-TOO <lsh20806172@gmail.com>
2023-10-16 09:44:31 +0900

    깃 테스트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { useSelector } from "react-redux"
+import MainPage from './components/MainPage'
 
 
 function App() {
   let test = useSelector((state) => { return state } )
   return (
     <div>
+    <header> <p>1번</p> <p>2번</p> <p>3번</p> <p>3번</p> </header>
+
+    <MainPage/>
+    
+
+
     <p>{test.title}</p>
     <p>우리팀은 : {test.user}</p>
+    
+    
+ 
+ 
   </div>
+
+
   );
 }
 
---
+++ src/components/UserJoin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Join() {
+  return (
+    <div>회원가입페이지</div>
+  )
+}
+
+export default Join

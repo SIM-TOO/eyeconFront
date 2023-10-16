99bbd2ef0ae104e98157d3ada36af5816233df2f
SIM-TOO <lsh20806172@gmail.com>
2023-10-16 12:41:45 +0900

    1016_12:41 이승훈_테일윈드 환경설정

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -9,6 +9,9 @@ function Join() {
     <div>
     <p>--------------</p>
     <p>메인</p>
+    <h1 className="text-5xl font-bold underline">
+      Hello world!
+    </h1>
     <p>{test.title}</p>
     <p>우리팀은 : {test.user}</p>
     <p>--------------</p>

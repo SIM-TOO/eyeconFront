3cb5f2a6fdc45d6c24d39eb943f3ac589848c39f
chgo0506 <chgo0506@gmail.com>
2023-10-16 09:55:43 +0900

    1016_09:55_류상지_테스트

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header'
 function App() {
   
   return (
-<<<<<<< HEAD
     <div>
     <Header/>
 
@@ -19,25 +18,6 @@ function App() {
   </div>
 
 
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2>지홍 왔다감</h2>
-      </header>
-    </div>
->>>>>>> 3722f66ec5fb71cea211b6915762d79c1cc9ef23
   );
 }
 

5c9d169bb4733a8786e57383d3e426272a654f9a
SIM-TOO <lsh20806172@gmail.com>
2023-10-16 12:19:14 +0900

    1016_12:19_이승훈_Pay 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage'
 import UserJoin from './components/UserJoin'
-
+import Pay from './components/Pay'
 function App() {
   
   return (
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/join" element={<UserJoin />} />
+          <Route path="/pay" element={<Pay />} />
        </Routes>
 
       </div>

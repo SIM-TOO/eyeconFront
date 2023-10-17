648b200fd1ae3fa27f9ddccffff649963f491c12
vlfgus <vlfgus321@gmail.com>
2023-10-17 11:30:10 +0900

    1017_1129_문필현_수정

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 function Pay() {
-
-    
-
 
     return (
         <div>
@@ -13,15 +9,17 @@ function Pay() {
             <img src="https://i.ibb.co/7R0ySQh/logo.png" alt="logo" border="0"></img>
           </div>
         </header>
-        <div class="w-64 h-64">
+        <div class="flex justify-center items-center">
+            <div class="relative w-1/3">
          <img src="https://i.ibb.co/5TcWgzd/price.png" alt="price" border="0"/>
-         <img src="https://i.ibb.co/XsRtS66/REASONABLE-GOOD-PRICE.jpg" alt="REASONABLE-GOOD-PRICE" border="0"/>
+         <img src="https://i.ibb.co/XsRtS66/REASONABLE-GOOD-PRICE.jpg" alt="REASONABLE-GOOD-PRICE" border="0" class="absolute top-0 left-0 w-15 h-10"/>
          </div>
-         <div class="h-screen">
+         <div class="w-1/3">
          <img src="https://i.ibb.co/vmWSTyD/content-inside.jpg" alt="content-inside" border="0" class="mx-auto"/>
          </div>
-         <div>
-         <h3>PRICING</h3>
+         </div>
+         <div class="text-center">
+         <h3 class="text-blue-700 fontweight-light">PRICING</h3>
          <h1>서비스 플랜</h1>
          <p className="text-center p-4 font-semibold">eyecon은 소비자의 데이터를 정밀하게 분석해 매대의 시선 배치를 정확히 분석합니다.
             효과적인 마케팅 방법을 고민 중인 소매업자를 위한 다양한 플랜이 준비되어 있습니다.

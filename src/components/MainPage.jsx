5b3a952a1d8d13a7083a3486e9fd0fd19075a15f
SIM-TOO <lsh20806172@gmail.com>
2023-10-17 19:01:30 +0900

    1017_19:01_이승훈_헤더 작업 진행 커밋 테스트

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -8,14 +8,7 @@ function MainPage() {
     return (
     <div>
     <Header/>
-    <p>--------------</p>
-    <p>메인</p>
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
-    <p>{test.title}</p>
-    <p>우리팀은 : {test.user}</p>
-    <p>--------------</p>
+
     </div>
   )
 }

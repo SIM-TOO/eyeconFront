7184c88aa74d5e13a044ad406198580523c663e6
vlfgus <vlfgus321@gmail.com>
2023-10-18 14:36:22 +0900

    1018_1436_문필현 이벤트 완성

--- src/components/PayPage/PlanCard.jsx
+++ src/components/PayPage/PlanCard.jsx
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 
 
 function PlanCard() {
-  const [isHovered, setIsHovered] = useState(false);
+  const [hoveredBox, setHoveredBox] = useState(null);
   const image1 = "https://i.ibb.co/pRDck3F/Feature.jpg"; // 초록색 체크표시
   const image2 = "https://i.ibb.co/b1xHdYc/icon.png"; // 흰색 체크표시
-  const [changeimg, setChangeimg] = useState(image1);
 
-  const handleHover = () => {
-    setIsHovered(!isHovered);
-  };
-
-  useEffect(() => {
-    setChangeimg(isHovered ? image1 : image2);
-  }, [isHovered]);
-
-  const Event = (e) => {
-    e.currentTarget.classList.toggle("hover:bg-[#15c3a7]");
-  }
-  const handleMouseEnter = (e) => {
-    Event(e);
-    handleHover();
+  const handleMouseEnter = (box) => {
+    setHoveredBox(box);
   };
 
   return (
@@ -30,10 +17,12 @@ function PlanCard() {
 
       {/* BASIC 시작 부분 */}
       {/* ↓↓↓ 마우스 올렸을때 호버 배경색 변경되는 이벤트 ↓↓↓ */}
-      <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] 
-      overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9]"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseEnter}>
+      <div
+        className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] 
+        overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] ${hoveredBox === "basic" ? "hover:bg-[#15c3a7]" : ""}`}
+        onMouseEnter={() => handleMouseEnter("basic")}
+        onMouseLeave={() => handleMouseEnter(null)}
+      >
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
             <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
@@ -44,19 +33,19 @@ function PlanCard() {
         </div>
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "basic" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               5 Coins
             </p>
           </div>
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "basic" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               10 Shares
             </p>
           </div>
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "basic" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               Custom Domains
             </p>
@@ -68,10 +57,10 @@ function PlanCard() {
 
       {/* Premium 시작부분 */}
       {/* ↓↓↓ 마우스 올렸을때 호버 배경색 변경되는 이벤트 ↓↓↓ */}
-      <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px]
-      overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] "
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseEnter}>
+      <div className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px]
+        overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] ${hoveredBox === "premium" ? "hover:bg-[#15c3a7]" : ""}`}
+        onMouseEnter={() => handleMouseEnter("premium")}
+        onMouseLeave={() => handleMouseEnter(null)}>
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
             <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
@@ -82,19 +71,19 @@ function PlanCard() {
         </div>
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "premium" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               15 Coins
             </p>
           </div>
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "premium" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               125 Shares
             </p>
           </div>
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "premium" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               Analytics
             </p>
@@ -105,10 +94,10 @@ function PlanCard() {
 
       {/* Pro 부분 */}
       {/* ↓↓↓ 마우스 올렸을때 호버 배경색 변경되는 이벤트 ↓↓↓ */}
-      <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] 
-      overflow-hidden gap-2.5 p-2.5 rounded-lg bg-white border border-[#d9d9d9]"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseEnter}>
+      <div className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] 
+        overflow-hidden gap-2.5 p-2.5 rounded-lg bg-white border border-[#d9d9d9] ${hoveredBox === "pro" ? "hover:bg-[#15c3a7]" : ""}`}
+        onMouseEnter={() => handleMouseEnter("pro")}
+        onMouseLeave={() => handleMouseEnter(null)}>
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
             <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
@@ -119,19 +108,19 @@ function PlanCard() {
         </div>
         <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "pro" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               30 Coins
             </p>
           </div>
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "pro" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
               Unlimited
             </p>
           </div>
           <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={changeimg} alt="Feature" border="0" />
+            <img src={hoveredBox === "pro" ? image2 : image1} alt="Feature" border="0" />
             <p className="flex-grow-0 flex-shrink-0 w-[156px] text-xl font-semibold text-left text-black">
               Custom Brand
             </p>

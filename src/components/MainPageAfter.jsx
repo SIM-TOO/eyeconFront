19cb16118ff1f9cb1807704e75e9147d2e03f58a
SIM-TOO <lsh20806172@gmail.com>
2023-10-18 15:09:19 +0900

    1018_15:08_이승훈_로그인,회원가입,메인,메인애프터 헤더 라인 위치 통일

--- src/components/MainPageAfter.jsx
+++ src/components/MainPageAfter.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
+
 function MainPageAfter() {
   return (
+    // 배경화면 설정
     <div
-      className='bg-cover bg-no-repeat h-screen'
+      className='h-screen'
       style={{
         backgroundImage: 'url("https://i.ibb.co/Q8M7ycH/Kakao-Talk-20231018-121457122.png")',
         backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
+        backgroundSize: 'cover'
       }}>
+        
+      {/* 헤더 */}
       <Header />
 
-      <div
-        className='flex flex-col items-center justify-center h-screen'
-      >
+      {/* 내부박스 */}
+      <div className='flex flex-col items-center' style={{ height: 'calc(100vh - 120px)' }}>
 
         <div
-          className="w-1/2 h-1/2 rounded-2xl"
+          className="w-[70%] h-[90%] rounded-2xl"
           style={{
             background: 'linear-gradient(179.97deg, #fff 0.05%, rgba(255,255,255,0) 99.98%)',
             boxShadow: '0px 4px 30px 0 rgba(190,190,190,0.47)',
-          }}
-        ></div>
+          }}>
+
+            {/* 여기에 컴포넌트 실행 해야됨!!!! */}
+
+        </div>
 
 
       </div>
     </div>
-  )
+
+  );
 }
 
-export default MainPageAfter
+export default MainPageAfter;

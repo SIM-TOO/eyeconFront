09fbc5ec4d989245657137d67fe55e81001aae17
chgo0506 <chgo0506@gmail.com>
2023-10-18 11:53:06 +0900

    1018_11:52_류상지 링크 추가 및 헤더 수정중

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,10 +38,10 @@ function Header() {
             {/* 서비스 시작 버튼 & 회원 가입 */}
             <div className="col-span-6 flex items-center justify-end hidden md:flex">
               <div className="flex flex-wrap">
-                <Link to="/#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 bg-white border border-[#d9dbe9] rounded-[80px] h-[52px] flex items-center justify-center mb-2 md:mb-0">
+                <Link to="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 bg-white border border-[#d9dbe9] rounded-[80px] h-[52px] flex items-center justify-center mb-2 md:mb-0">
                   서비스 시작하기
                 </Link>
-                <Link to="/#" className="flex items-center justify-center text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 h-[52px] rounded-[80px] bg-[#15c3a7] border border-[#d9dbe9] mb-2 md:mb-0">
+                <Link to="/join" className="flex items-center justify-center text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 h-[52px] rounded-[80px] bg-[#15c3a7] border border-[#d9dbe9] mb-2 md:mb-0">
                   회원가입
                 </Link>
               </div>
@@ -51,23 +51,52 @@ function Header() {
             <div className="col-span-2 relative md:hidden">
               <button
                 onClick={toggleMenu}
-                className=" flex items-center justify-center text-white hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 h-[52px] rounded-[80px] bg-[#15c3a7] border border-[#d9dbe9] mb-2 md:mb-0">
-                X
-              </button>
-              {menuOpen && (
-                <div className="absolute mt-2 py-5 px-5 bg-gray-800 rounded-md">
-                  <ul className="text-white">
-                    <li>Menu Item 1</li>
-                    <li>Menu Item 2</li>
-                    <li>Menu Item 3</li>
-                  </ul>
-                </div>
-              )}
-            </div>
+                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
 
+
+                <svg
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  class="hidden w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+
+              </button>
+            </div>
+            <div>
+            {menuOpen && (
+              <div className="absolute justify-between items-center w-full lg:w-auto lg:order-1">
+                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 font-light w-full">
+                  <li className="block py-2 pr-4 pl-3 text-white rounded bg-[#15c3a7] lg:bg-transparent lg:text-gray-600 lg:p-0 dark:text-white transition-transform hover:font-semibold hover:text-blue-500"
+                    aria-current="page">About</li>
+                  <li className="block py-2 pr-4 pl-3 text-white rounded bg-[#999999] lg:bg-transparent lg:text-gray-600 lg:p-0 dark:text-white transition-transform hover:font-semibold hover:text-blue-500"
+                    aria-current="page">Pricing</li>
+                </ul>
+              </div>
+            )}
           </div>
-        </nav>
-      </header>
+
+        </div>
+      </nav>
+    </header>
     </div >
   );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,12 +3,17 @@ import { useSelector } from "react-redux"
 import Header from './Header'
 function MainPage() {
 
-    let test = useSelector((state) => { return state } )
+  let test = useSelector((state) => { return state })
 
-    return (
+  return (
     <div>
-    <Header/>
-
+      <Header />
+      <main className="mx auto">
+        <p className="w-[1045.29px] h-[195.71px] text-[220px] font-bold text-left uppercase text-stroke-1 text-[#33E0C4] fill-[#ffffff]">
+          테스트
+        </p>
+        ;<div className="container mx-11 p-5 mx auto"></div>
+      </main>
     </div>
   )
 }

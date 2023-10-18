399047912a3b8fdda784265303130a934f20bb91
SIM-TOO <lsh20806172@gmail.com>
2023-10-18 12:45:30 +0900

    1018_12:45_이승훈_로그인 페이지 위치 수정

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,8 +10,8 @@ const Login = () => {
     return (
         <div>
             <UserHeader />
-            <main className="container my-10 p-5 mx-auto">
-                <div className="grid md:grid-cols-2 gap-8">
+            <main>
+                <div className="container my-10 p-5 mx-auto grid md:grid-cols-2 gap-8">
                     <UserImage />
                     <div className="mx-auto lg:p-12 lg:min-w-[600px] p-0 min-w-[300px]">
                         <p className="text-3xl font-medium text-left text-black p-3">Sign in</p>

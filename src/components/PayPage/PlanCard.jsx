1a54a2dc918163d19e281b764a3a46aa8bab05e3
vlfgus <vlfgus321@gmail.com>
2023-10-19 14:17:26 +0900

    1019_1417_문필현_커밋

--- src/components/PayPage/PlanCard.jsx
+++ src/components/PayPage/PlanCard.jsx
@@ -33,37 +33,39 @@ function PlanCard() {
   };
 
   return (
-    <div className="flex justify-center items-start self-stretch flex-grow-0 flex-shrink-0 gap-8">
+    <div className="grid grid-cols-12 gap-4">
       {plans.map((plan, index) => (
-        <div
-          key={index}
-          className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] ${
-            hoveredBox === plan.title.toLowerCase() ? "hover:bg-[#15c3a7]" : ""
-          }`}
-          onMouseEnter={() => handleMouseEnter(plan.title.toLowerCase())}
-          onMouseLeave={() => handleMouseEnter(null)}
-        >
-          <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
-              <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
-                {plan.title}
-              </p>
+
+        <div className="col-span-2 p-4">
+          <div
+            key={index}
+            className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] ${hoveredBox === plan.title.toLowerCase() ? "hover:bg-[#15c3a7]" : ""
+              }`}
+            onMouseEnter={() => handleMouseEnter(plan.title.toLowerCase())}
+            onMouseLeave={() => handleMouseEnter(null)}
+          >
+            <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
+              <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
+                <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
+                  {plan.title}
+                </p>
+              </div>
+              <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-left text-black">{plan.price}</p>
             </div>
-            <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-left text-black">{plan.price}</p>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
+                {plan.features.map((feature, featureIndex) => (
+                  <div key={featureIndex} className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
+                    <img src={hoveredBox === plan.title.toLowerCase() ? image2 : image1} alt="Feature" border="0" />
+                    <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
+                      {feature}
+                    </p>
+                  </div>
+                ))}
+                <button type="submit">보내기</button>
+              </div>
+            </form>
           </div>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
-              {plan.features.map((feature, featureIndex) => (
-                <div key={featureIndex} className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-                  <img src={hoveredBox === plan.title.toLowerCase() ? image2 : image1} alt="Feature" border="0" />
-                  <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-                    {feature}
-                  </p>
-                </div>
-              ))}
-              <button type="submit">보내기</button>
-            </div>
-          </form>
         </div>
       ))}
     </div>
--- src/components/resultList/ListSet.jsx
+++ src/components/resultList/ListSet.jsx
@@ -1,37 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function List() {
   return (
-
-    // 마트 리스트 목록 & 리스트 추가 될때마다 버튼 생성
     <div className='grid grid-cols-12 gap-4'>
-
-      <div className='col-span-4 p-4 text-center w-[auto] h-[auto]' style={{ filter: "drop-shadow(0px 4px 4px rgba(0,0,0,0.25)" }}>
-
-
-        <div className=' w-[auto] h-[auto] rounded-[35px] bg-white' style={{ boxShadow: "1px 1px 1px 0 rgba(0,0,0,0.1", backgroundColor: "PALEGOLDENROD" }}>
-          <p className='text-5xl font-bold text-center text-black '>시선 분석 결과</p>
-          <div className='col-span-7 p-4 h-[auto] left-[auto] top-[auto] rounded-[20px] bg-[#15c3a7] text-center' style={{ boxShadow: "2px 6px 0 rgba(0,0,0,0.25)" }}>
+      <div className='col-span-4 p-4 text-center w-[auto] h-[auto]' style={{ filter: 'drop-shadow(0px 4px 4px rgba(0,0,0,0.25)' }}>
+        <div className='w-[auto] h-[auto] rounded-[35px] bg-white' style={{ boxShadow: '1px 1px 1px 0 rgba(0,0,0,0.1', backgroundColor: 'PALEGOLDENROD' }}>
+          <p className='text-5xl font-bold text-center text-black'>시선 분석 결과</p>
+          <div className='col-span-7 p-4 h-[auto] left-[auto] top-[auto] rounded-[20px] bg-[#15c3a7] text-center' style={{ boxShadow: '2px 6px 0 rgba(0,0,0,0.25)' }}>
             <p className='top-auto left-auto font-bold text-neutral-100' style={{ width: 'auto', whiteSpace: 'nowrap', overflow: 'hidden' }}>A마트</p>
           </div>
-          <div className='w-[auto] h-[auto]  overflow-hidden rounded-3x1 bg-[#f9f9f9]/[0.84] border-2 border-black/10'>
-            <p className='fext-2xl font-bold text-left text-[#535353]'>2023-10-19</p>
+          <div className='w-[auto] h-[auto] overflow-hidden rounded-3x1 bg-[#f9f9f9]/[0.84] border-2 border-black/10'>
+            <p className='text-2xl font-bold text-left text-[#535353]'>2023-10-19</p>
             <div className="flex justify-start items-start w-[auto] h-[auto] gap-2 p-2 rounded-[20px] border border-[#15c3a7]">
               <div className="w-[auto] h-[auto]">
                 <div className="w-[auto] h-[auto] left-[auto] top-[auto] rounded-[9px] bg-[#15c3a7]" />
-                <p className=" w-[auto] h-[auto] left-[auto] top-3 text-base font-medium text-left text-white">
-                  다운로드
-                </p>
+                <p className="w-[auto] h-[auto] left-[auto] top-3 text-base font-medium text-left text-white">다운로드</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
-
-  )
+  );
 }
 
 export default List;

ba49b4d7a73aaff090658ad2acfda5de65657391
vlfgus <vlfgus321@gmail.com>
2023-10-19 12:30:13 +0900

    1019_1230_문필현_PlanCard 코드 간결화

--- src/components/PayPage/PlanCard.jsx
+++ src/components/PayPage/PlanCard.jsx
@@ -1,12 +1,28 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 
+const plans = [
+  {
+    title: "Basic",
+    price: "￦25,000",
+    features: ["5 Coins", "10 Shares", "Custom Domains"],
+  },
+  {
+    title: "Premium",
+    price: "￦75,000",
+    features: ["15 Coins", "125 Shares", "Analytics"],
+  },
+  {
+    title: "Pro",
+    price: "￦150,000",
+    features: ["30 Coins", "Unlimited", "Custom Brand"],
+  },
+];
+
+const image1 = "https://i.ibb.co/pRDck3F/Feature.jpg";
+const image2 = "https://i.ibb.co/b1xHdYc/icon.png";
 
 function PlanCard() {
   const [hoveredBox, setHoveredBox] = useState(null);
-  const image1 = "https://i.ibb.co/pRDck3F/Feature.jpg"; // 초록색 체크표시
-  const image2 = "https://i.ibb.co/b1xHdYc/icon.png"; // 흰색 체크표시
-  const [submit, setSubmit] = useState("");
-
 
   const handleMouseEnter = (box) => {
     setHoveredBox(box);
@@ -14,132 +30,44 @@ function PlanCard() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
-
-    // PlanCard 전체부분
     <div className="flex justify-center items-start self-stretch flex-grow-0 flex-shrink-0 gap-8">
-
-      {/* BASIC 시작 부분 */}
-      {/* ↓↓↓ 마우스 올렸을때 호버 배경색 변경되는 이벤트 ↓↓↓ */}
-      <div className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9]
-       ${hoveredBox === "basic" ? "hover:bg-[#15c3a7]" : ""}`}
-        onMouseEnter={() => handleMouseEnter("basic")}
-        onMouseLeave={() => handleMouseEnter(null)}
-      
-      >
-        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
-            <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
-              Basic
-            </p>
+      {plans.map((plan, index) => (
+        <div
+          key={index}
+          className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] ${
+            hoveredBox === plan.title.toLowerCase() ? "hover:bg-[#15c3a7]" : ""
+          }`}
+          onMouseEnter={() => handleMouseEnter(plan.title.toLowerCase())}
+          onMouseLeave={() => handleMouseEnter(null)}
+        >
+          <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
+            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
+              <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
+                {plan.title}
+              </p>
+            </div>
+            <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-left text-black">{plan.price}</p>
           </div>
-          <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-left text-black">￦25,000</p>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
+              {plan.features.map((feature, featureIndex) => (
+                <div key={featureIndex} className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
+                  <img src={hoveredBox === plan.title.toLowerCase() ? image2 : image1} alt="Feature" border="0" />
+                  <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
+                    {feature}
+                  </p>
+                </div>
+              ))}
+              <button type="submit">보내기</button>
+            </div>
+          </form>
         </div>
-        <form onSubmit={handleSubmit}>
-        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "basic" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              5 Coins
-            </p>
-          </div>
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "basic" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              10 Shares
-            </p>
-          </div>
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "basic" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              Custom Domains
-            </p>
-          </div>
-        <button type="submit"> 보내기 </button>
-        </div>
-        </form>
-      </div>
-
-
-
-      {/* Premium 시작부분 */}
-      {/* ↓↓↓ 마우스 올렸을때 호버 배경색 변경되는 이벤트 ↓↓↓ */}
-      <div className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px]
-        overflow-hidden gap-2.5 p-2.5 rounded-lg border border-[#d9d9d9] ${hoveredBox === "premium" ? "hover:bg-[#15c3a7]" : ""}`}
-        onMouseEnter={() => handleMouseEnter("premium")}
-        onMouseLeave={() => handleMouseEnter(null)}>
-        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
-            <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
-              Premium
-            </p>
-          </div>
-          <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-left text-black">￦75,000</p>
-        </div>
-        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "premium" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              15 Coins
-            </p>
-          </div>
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "premium" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              125 Shares
-            </p>
-          </div>
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "premium" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              Analytics
-            </p>
-          </div>
-        </div>
-      </div>
-
-
-      {/* Pro 부분 */}
-      {/* ↓↓↓ 마우스 올렸을때 호버 배경색 변경되는 이벤트 ↓↓↓ */}
-      <div className={`flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-[260px] 
-        overflow-hidden gap-2.5 p-2.5 rounded-lg bg-white border border-[#d9d9d9] ${hoveredBox === "pro" ? "hover:bg-[#15c3a7]" : ""}`}
-        onMouseEnter={() => handleMouseEnter("pro")}
-        onMouseLeave={() => handleMouseEnter(null)}>
-        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 p-2.5">
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 relative gap-2.5 px-3 pt-1 rounded-lg border border-[#5d6a85]">
-            <p className="flex-grow-0 flex-shrink-0 text-sm font-semibold text-left uppercase text-black">
-              Pro
-            </p>
-          </div>
-          <p className="flex-grow-0 flex-shrink-0 text-5xl font-bold text-left text-black">￦150,000</p>
-        </div>
-        <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 overflow-hidden gap-2.5 p-2.5">
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "pro" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              30 Coins
-            </p>
-          </div>
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "pro" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 text-xl font-semibold text-center text-black">
-              Unlimited
-            </p>
-          </div>
-          <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 h-[38px] relative gap-2.5">
-            <img src={hoveredBox === "pro" ? image2 : image1} alt="Feature" border="0" />
-            <p className="flex-grow-0 flex-shrink-0 w-[156px] text-xl font-semibold text-left text-black">
-              Custom Brand
-            </p>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
-  )
-};
+  );
+}
 
-
-
-export default PlanCard
+export default PlanCard;
--- src/components/resultList/ListSet.jsx
+++ src/components/resultList/ListSet.jsx
@@ -11,8 +11,8 @@ function List() {
 
         <div className=' w-[auto] h-[auto] rounded-[35px] bg-white' style={{ boxShadow: "1px 1px 1px 0 rgba(0,0,0,0.1", backgroundColor: "PALEGOLDENROD" }}>
           <p className='text-5xl font-bold text-center text-black '>시선 분석 결과</p>
-          <div className='col-span-7 p-4 h-[auto] left-[200px] top-[190px] rounded-[20px] bg-[#15c3a7] text-center' style={{ boxShadow: "2px 6px 0 rgba(0,0,0,0.25)" }}>
-            <p className='top-auto left-auto font-bold text-right text-neutral-100' style={{ width: 'auto', whiteSpace: 'nowrap', overflow: 'hidden' }}>A마트</p>
+          <div className='col-span-7 p-4 h-[auto] left-[auto] top-[auto] rounded-[20px] bg-[#15c3a7] text-center' style={{ boxShadow: "2px 6px 0 rgba(0,0,0,0.25)" }}>
+            <p className='top-auto left-auto font-bold text-neutral-100' style={{ width: 'auto', whiteSpace: 'nowrap', overflow: 'hidden' }}>A마트</p>
           </div>
           <div className='w-[auto] h-[auto]  overflow-hidden rounded-3x1 bg-[#f9f9f9]/[0.84] border-2 border-black/10'>
             <p className='fext-2xl font-bold text-left text-[#535353]'>2023-10-19</p>

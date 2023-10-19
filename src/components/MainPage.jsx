3863107076eb508f0ab6363975e037d4c5d7ca38
SIM-TOO <lsh20806172@gmail.com>
2023-10-19 16:15:52 +0900

    1019_16:15_이승훈_브런치 테스트용

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import Intro01 from './introPage/Intro01'
 import Intro02 from './introPage/Intro02'
 import Intro03 from './introPage/Intro03'
+import Intro04 from './introPage/Intro04'
 
 function MainPage() {
   return (
@@ -11,6 +12,7 @@ function MainPage() {
       <Intro01/>
       <Intro02/>
       <Intro03/>
+      <Intro04/>
 
     </div>
   )

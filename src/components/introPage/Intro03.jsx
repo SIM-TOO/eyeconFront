435077d4f545aae4fa4b13d8bad14722d04e0ca4
SIM-TOO <lsh20806172@gmail.com>
2023-10-19 14:36:57 +0900

    1019_14:37_이승훈_인트로01 버그 및 크기 수정

--- src/components/introPage/Intro03.jsx
+++ src/components/introPage/Intro03.jsx
@@ -5,13 +5,13 @@ const Intro03 = () => {
         <div>
             {/* 디자인용 박스*/}
             <div className="container mx-auto grid grid-cols-12 gap-4 max-w-screen-xl p-3">
-                <div className="col-span-4 md:col-span-5" />
-                <div className="col-span-4 md:col-span-2 border border-[#696c73] rounded-lg">
-                    <p className="text-center text-xm  md:text-2xl font-bold text-[#696c73] p-2">
+                <div className="col-span-3 md:col-span-5" />
+                <div className="col-span-6 md:col-span-2 border border-[#696c73] rounded-lg">
+                    <p className="text-center whitespace-nowrap text-sm  md:text-2xl font-bold text-[#696c73] p-2">
                         OUR SERVICES
                     </p>
                 </div>
-                <div className="col-span-4 md:col-span-5" />
+                <div className="col-span-3 md:col-span-5" />
             </div>
         </div>
     )

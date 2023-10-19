833e92db9d10f6801f791369ed985248efb37d84
philosokey-M <wlghd99589@gmail.com>
2023-10-19 19:13:25 +0900

    1019_19:12_김지홍_gpt테스트

---
+++ src/components/testPage/gpttest.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const gpttest = () => {
+ 
+    // const getgptjason =()=>{
+    //     axios.
+    // }
+ 
+    return (
+    <div>gpttest</div>
+    
+  )
+}
+
+export default gpttest

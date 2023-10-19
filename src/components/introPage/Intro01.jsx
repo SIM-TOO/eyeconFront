2397eba4ca7fdb5141c1e014b5c449cd6cc9419f
SIM-TOO <lsh20806172@gmail.com>
2023-10-19 20:05:51 +0900

    1018_20:05_이승훈_결제창 수리중

--- src/components/introPage/Intro01.jsx
+++ src/components/introPage/Intro01.jsx
@@ -10,12 +10,14 @@ const Intro01 = () => {
           className="h-auto pointer-events-none"
         />
       </div>
+
+
       <div className="container mx-auto grid grid-cols-12 gap-4 max-w-screen-xl">
         <div className="col-span-12 md:col-span-6">
           {/* 첫번째 줄 내용 겹치는 박스 */}
           <div className="box md:absolute md:top-40 relative p-6 sm:p-3 md:p-5">
-            <p className="text-4xl md:text-6xl font-bold">WELCOME TO <br/>
-            EYECON</p>
+            <p className="text-4xl md:text-6xl font-bold">WELCOME TO <br />
+              EYECON</p>
             <p className="relative">
               <span className="relative z-10 text-4xl md:text-6xl font-bold">with AI CHATBOT</span>
               <div className="absolute w-[50px] md:w-[470px] h-[20px] bg-[#15c3a7] top-[42px] rounded-full md:block hidden"></div>
@@ -24,13 +26,13 @@ const Intro01 = () => {
 
           {/* 두번째 줄 내용 겹치는 박스 */}
           <div class="py-5 md:py-40 p-6 sm:p-3 text-base sm:text-lg">
-    시선 분석 AI를 통해 매대를 정밀하게 분석하고 <br/> 소비자의 시선을
-    예측해보세요. <br/> 소비자의 시선이 향하는 곳에 매출이 있습니다. <br/> Where the
-    eyes go, sales are there <br/>우리는 챗봇을 통해 이용자들의 요구사항을
-    확인합니다.  <br/>서비스에 필요한 정보들을 챗봇과 해결해보세요. <br/>상권, 성별, 연령을 바탕으로 최적의 시선배치를
-    도와줍니다. <br/> 우리는 챗봇 AI를 통해 더욱 정확해지고,
-    빨라졌습니다.
-</div>
+            시선 분석 AI를 통해 매대를 정밀하게 분석하고 <br /> 소비자의 시선을
+            예측해보세요. <br /> 소비자의 시선이 향하는 곳에 매출이 있습니다. <br /> Where the
+            eyes go, sales are there <br />우리는 챗봇을 통해 이용자들의 요구사항을
+            확인합니다.  <br />서비스에 필요한 정보들을 챗봇과 해결해보세요. <br />상권, 성별, 연령을 바탕으로 최적의 시선배치를
+            도와줍니다. <br /> 우리는 챗봇 AI를 통해 더욱 정확해지고,
+            빨라졌습니다.
+          </div>
 
           {/* 버튼 만드는 자리 */}
           <div className="container mx-auto grid grid-cols-12">

336658755eb5a241d0e349a9cd317e4708345bb6
SIM-TOO <lsh20806172@gmail.com>
2023-10-19 09:06:29 +0900

    1019_09:05_이승훈_env파일 테스트

--- src/hook/userPage/useUserJoin.js
+++ src/hook/userPage/useUserJoin.js
@@ -1,8 +1,9 @@
 import { useRef } from 'react';
 
+
 const useUserJoin = () => {
   const formRef = useRef(null);
-
+const url = process.env.REACT_APP_MASTER_URL
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(formRef.current);
@@ -17,6 +18,7 @@ const useUserJoin = () => {
     const place1 = formData.get('place1');
     const place2 = formData.get('place2');
 
+    //console.log(key);
     // 확인용 콘솔log
     // console.log(email);
     // console.log(password);
@@ -25,7 +27,17 @@ const useUserJoin = () => {
     // console.log(place1);
     // console.log(place2);
 
-    // 
+
+
+    // axios.post(`${url}:`, {
+    
+    // // 객체 만들기
+    
+    
+    
+    // }, [, config])
+
+
 
     // json 묶는  로직 + axios 방식
     // 여기서 DB확인용 로직 작성하면 될듯? ^^

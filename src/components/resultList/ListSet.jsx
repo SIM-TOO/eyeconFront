38fe7f3c4910824a1efdea4ccf8df6209fcec76e
vlfgus <vlfgus321@gmail.com>
2023-10-20 19:05:16 +0900

    1020_1705_문필현_그리드 및 레이아웃 재설정

--- src/components/resultList/ListSet.jsx
+++ src/components/resultList/ListSet.jsx
@@ -1,41 +1,73 @@
 import React from 'react';
 
-function List() {
+function ListItem({ date, img1, img2, description }) {
   return (
+    <div className='grid grid-cols-3 border-2 gap-4'>
+      <p className='col-span-4 text-1xl text-2xl text-left text-black'>{date}</p>
+      <div className='col-span-1 border border-[#15c3a7]' id="item1">
+        <img src={img1} alt="image" border="0" className='object-contain w-[auto] h-[auto]' />
+      </div>
+      <div className='col-span-1 border border-[#15c3a7]' id='item2'>
+        <img src={img2} alt="heatmap" border="0" className='object-contain w-[auto] h-[auto]' />
+      </div>
+      <div className='col-span-1'>
+        <p className='text-base text-[#313131] text-center'>{description}</p>
+      </div>
+    </div>
+  );
+}
 
+function List() {
+  const items = [
+    {
+      date: "2023-10-19",
+      img1: "https://i.ibb.co/wKLDFT2/image-36.png",
+      img2: "https://i.ibb.co/wYpgY2J/Rectangle.png",
+      description: "광주 북구 공장단지 상권 40대 남성을 타겟으로 한 메대 시선분석 결과입니다."
+    },
+    // 추가 항목들...
+  ];
 
-    <div className='grid grid-cols-12 gap-4'>
+  return (
+    <div className='grid grid-cols-12 gap-4 bg-light-blue'>
       <div className='col-span-3' />
 
       <div class="col-span-6 flex flex-col justify-center items-center rounded-[25px]" style={{ width: "100%", height: "100%", boxShadow: "1px 1px 1px 0 rgba(0,0,0,0.25)" }}>
-        <div className='w-[25%] h-[auto] rounded-[20px] bg-[#15c3a7] item-left' style={{ boxShadow: "2px 6px 4px 0 rgba(0.0.0.25)" }}>
-          <p className='text-2xl font-bold text-center text-neutral-700'>A마트</p>
-        </div>
-        <div className='w-[auto] h-[auto] bg-[A9FFF1]'>
+        <div className='w-[auto] h-[auto] bg-white'>
+          <div className='w-[25%] h-[auto] rounded-[20px] bg-[#15c3a7] item-left' style={{ boxShadow: "2px 6px 4px 0 rgba(0.0.0.25)" }}>
+            <p className='text-2xl font-bold text-center text-neutral-700'>A마트</p>
+          </div>
           <p className='text-5xl font-bold text-center text-black'> 시선 분석 결과 </p>
           <p className='text-2xl font-bold text-right text-black'>↓ 최신순</p>
-          <div className='grid grid-cols-4 border-2 gap-3'>
-          <p className='col-span-1 text-1xl text-2xl text-left text-black'>2023-10-19</p>
-            <div className='col-span-1 border border-[#15c3a7]'>
-              <img src="https://i.ibb.co/wKLDFT2/image-36.png" alt="image-36" border="0" className='object-contain w-[auto] h-[auto]' />
-            </div>
-            <div className='col-span-1 border border-[#15c3a7]'>
-              <img src="https://i.ibb.co/wYpgY2J/Rectangle.png" alt="Rectangle" border="0" className='object-contain w-[auto] h-[auto]' />
+          {items.map(item => <ListItem {...item} />)}
+
+
+          <div className='grid grid-cols-3 items-start'>
+            <div className='col-span-1'>
+              <button className="text-sm md:text-2xl text-center text-white rounded-[10px] bg-[#15c3a7] whitespace-nowrap w-[65%] h-[auto] mb-4">
+                다운로드
+                <img src="https://i.ibb.co/s38LqgC/Vector.png" alt="Vector" border="0" className='w-[auto] h-[auto]' />
+              </button>
             </div>
             <div className='col-span-1'>
-              <p className='text-base text-[#313131] text-center'>광주 북구 공장단지 상권 40대 남성을 타겟으로 한 메대 시선분석 결과입니다.</p>
+              <button className="text-sm md:text-2xl text-center text-white rounded-[10px] bg-[#15c3a7] whitespace-nowrap w-[65%] h-[auto]">
+                다운로드
+                <img src="https://i.ibb.co/s38LqgC/Vector.png" alt="Vector" border="0" />
+              </button>
             </div>
+            <div className='col-span-1' />
           </div>
-          <button className="text-sm  md:text-2xl text-center text-white px-[35px] py-[15px] rounded-[10px] bg-[#15c3a7] whitespace-nowrap ">
-            다운로드
-            <img src="https://i.ibb.co/s38LqgC/Vector.png" alt="Vector" border="0" />
-          </button>
+
+
+
+
+          <div className='col-span-1' />
         </div>
       </div>
 
-
       <div className='col-span-3' />
-    </div >
+    </div>
+
   );
 }
 

c11d9403f8b20681fd5e3e883957bf6071c1d67c
philosokey-M <wlghd99589@gmail.com>
2023-10-20 19:18:23 +0900

    1020_1918_김지홍_충돌해결2

--- src/App.js
+++ src/App.js
@@ -10,11 +10,8 @@ import { AddressContext } from './context/AddressContext';
 import MainPageAfter from './components/MainPageAfter';
 import ResultPage from './components/ResultPage';
 import MainPageAfterTest from './components/MainPageAfterTest';
-<<<<<<< HEAD
 import Gpttest1 from './components/testPage/Gpttest1';
-=======
 import MainPageAfterPay from './components/MainPageAfterPay';
->>>>>>> e6cb633b0e575d0dd9a073de90c3f2f80289dc0e
 
 function App() {
 const [company, setCompany] = useState({})  

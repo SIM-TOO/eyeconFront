aa6d09e80cadc29741a7cfc89423b9a285a2482a
SIM-TOO <lsh20806172@gmail.com>
2023-10-20 18:57:51 +0900

    1020_18:57_이승훈_로그인후 메인페이지 결제페이지 완성

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { AddressContext } from './context/AddressContext';
 import MainPageAfter from './components/MainPageAfter';
 import ResultPage from './components/ResultPage';
 import MainPageAfterTest from './components/MainPageAfterTest';
+import MainPageAfterPay from './components/MainPageAfterPay';
 
 function App() {
 const [company, setCompany] = useState({})  
@@ -32,6 +33,7 @@ const inAddressContext={
           <Route path="/login" element={<Login />} />
           <Route path="/place" element={<Place/>}/>
           <Route path='/ResultPage' element={<ResultPage/>} />
+          <Route path='/mainpay' element={<MainPageAfterPay/>} />
        </Routes>
       </AddressContext.Provider>
 

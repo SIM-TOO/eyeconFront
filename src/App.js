cc0bfe3449d16219f61f40dd13b78214542dd295
philosokey-M <wlghd99589@gmail.com>
2023-10-20 19:16:43 +0900

    1020_19:15_김지홍_gpttest 채팅까지 끝

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { AddressContext } from './context/AddressContext';
 import MainPageAfter from './components/MainPageAfter';
 import ResultPage from './components/ResultPage';
 import MainPageAfterTest from './components/MainPageAfterTest';
+import Gpttest1 from './components/testPage/Gpttest1';
 
 function App() {
 const [company, setCompany] = useState({})  
@@ -32,6 +33,7 @@ const inAddressContext={
           <Route path="/login" element={<Login />} />
           <Route path="/place" element={<Place/>}/>
           <Route path='/ResultPage' element={<ResultPage/>} />
+          <Route path='/gpttest' element={<Gpttest1/>} />
        </Routes>
       </AddressContext.Provider>
 

a4db48b1febe55df99c6e19eb2bdd158231efb5b
chgo0506 <chgo0506@gmail.com>
2023-10-21 14:10:51 +0900

    1021_14:10_류상지_로그인, 회원가입 이미지 수정

--- src/components/userPage/UserImage.jsx
+++ src/components/userPage/UserImage.jsx
@@ -5,12 +5,13 @@ import lottieJson from '../../lottie/login.json';
 function UserImage() {
     return (
         <div>
-            <div className="hidden md:flex items-center ml-40">
+            <div className="hidden md:flex items-center ml-40 ">
                 <Lottie
                     loop
                     animationData={lottieJson}
                     play
                     style={{ width: '100%', maxWidth: '1000px' }}
+                    className='absolute top-20 left-0'
                 />
             </div>
         </div>

8af090146ea8c3317e2da51b1a5a0bdaf7ca5bf0
SIM-TOO <lsh20806172@gmail.com>
2023-10-23 18:31:34 +0900

    1022_18:30_이승훈_헤더애프터 컴포넌트 잔여코인 확인용 함수 완성

--- src/hook/mainPage/useCoin.js
+++ src/hook/mainPage/useCoin.js
@@ -1,16 +1,22 @@
 import axios from "axios";
 
 const useCoin = () => {
+    const url = process.env.REACT_APP_MASTER_URL;
 
-// const userInfo = localStorage.getItem("key")  // 일단 토큰 값 가져오기
+    const getCoinInfo = async (email) => {
+        try {
+            const response = await axios.get(`${url}/user/getCoin`, { params: { email: email } });
+            if (response && response.data) {
+                console.log(response)
+                return response.data.coin;
+            }
+        } catch (error) {
+            console.error("에러 내용:", error);
+        }
+        return null;
+    };
 
-    const removeCoin = async (event) => {
-        axios.post(`${url}/user/useCoin`, {email: "example@example.com"});
-        
-    }
-
-    return null;
-}
-
+    return getCoinInfo;
+};
 
 export default useCoin;
--- src/store.js
+++ src/store.js
@@ -1,18 +1,12 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-let title = createSlice({
-  name : 'title',
-  initialState : '스마트인재개발원 마지막 프로젝트'
-})
+let coinSlice = createSlice({
+  name: 'coin',
+  initialState: '',
+  reducers: {
+    setCoin: (state, action) => action.payload,
+  },
+});
 
-let user = createSlice({
-    name : 'user',
-    initialState : '지홍팀'
-  })
-
-export default configureStore({
-  reducer: {
-    title : title.reducer,
-    user : user.reducer,
-  }
-})
+export const { setCoin } = coinSlice.actions;
+export default coinSlice.reducer;

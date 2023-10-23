6987de5cc713319e884585c68bec06628fcd245a
SIM-TOO <lsh20806172@gmail.com>
2023-10-23 15:13:55 +0900

    1022_15:13_이승훈_서비스 이미지 기능 구현 완료

--- src/components/mainPage/technology/ImgDrop.jsx
+++ src/components/mainPage/technology/ImgDrop.jsx
@@ -1,43 +1,64 @@
-import React, { useState, useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
-function ImgDrop({ onUploadSuccess }) {
-  const [imageSrc, setImageSrc] = useState(null);  // 이미지의 URL 또는 base64 데이터를 저장할 상태 변수
+function ImgDrop({ onUploadSuccess, uploadedImage, uploadedImageSend, onUploadComplete }) {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [beforeimg, setBeforeimg] = useState(null);
+
+  // 이미지가 띄워진 상태 확인
+  useEffect(() => {
+    if (uploadedImage === null) {
+      setImageSrc(null);
+    }
+  }, [uploadedImage]);
+
+
+  // 이미지 서버로 업로드
+  useEffect(() => {
+    if (uploadedImageSend) {
+      const url = process.env.REACT_APP_MASTER_URL;
+      const formData = new FormData();
+
+      // 이미지의 실제 데이터
+      formData.append('Base64Data', uploadedImage);
+
+      // 이미지 이름 데이터
+      formData.append('beforeimg', beforeimg);
+
+      // 이상없음
+      // console.log(formData.get('Base64Data'));
+      // console.log(formData.get('beforeimg'));
+
+      axios.post(`${url}/test.do`, formData)
+        .then(response => {
+          // 전송 성공
+          console.log("전송 성공");
+          onUploadComplete(true);
+        })
+        .catch(error => {
+          // 전송 실패
+          console.error("전송을 실패 했습니다 에러 내용 :", error);
+          onUploadComplete(false);
+        });
+    }
+  }, [uploadedImageSend]);
 
   const onDrop = useCallback((acceptedFiles) => {
-    const url = process.env.REACT_APP_MASTER_URL;
-    console.log("이미지 업로드 시도");
-
     // 이미지 미리보기 설정
     const file = acceptedFiles[0];
+    setBeforeimg(file.name);
     const reader = new FileReader();
-
     reader.onload = (event) => {
       setImageSrc(event.target.result);
       if (onUploadSuccess) {
         onUploadSuccess(event.target.result);
       }
     };
-
     reader.readAsDataURL(file);
-
-    // // 이미지 서버로 업로드
-    // const formData = new FormData();
-    // formData.append('beforeimg', acceptedFiles[0].path);  // 파일 경로를 'beforeimg' 키로 추가
-
-    // // 테스트 출력
-    // console.log("이미지의 정보는 : ", formData);
-    // console.log("이미지의 이름 정보는 : ", formData.get('beforeimg'));
-
-    // try {
-    //   axios.post(`${url}/test.do`, formData);
-    // } catch (error) {
-    //   console.error("전송을 실패 했습니다 에러 내용 :", error);
-    // }
-
   }, []);
 
+  // 업로드 파일 조건 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     multiple: false, // 한 번에 여러 파일을 받을 수 없도록 설정
@@ -46,15 +67,16 @@ function ImgDrop({ onUploadSuccess }) {
     },  // 이미지만 허용
   });
 
+
   return (
     <div {...getRootProps()} className="col-span-12 p-3 border-2 border-gray-300 rounded-md hover:border-blue-500 cursor-pointer h-[100%] flex items-center justify-center">
+      {/* 이미지 업로드 박스 */}
       <input {...getInputProps()} />
       {
         imageSrc ?
           <div className="w-full h-72 flex items-center justify-center">
             <img src={imageSrc} alt="Uploaded Preview" className="max-h-full object-contain" />
           </div>
-
           :
           <>
             <p className="hidden md:block opacity-50 font-medium text-[#190e61]">
@@ -62,7 +84,6 @@ function ImgDrop({ onUploadSuccess }) {
               <br />
               이곳에 위치시키세요
             </p>
-
             <p className="block md:hidden opacity-50 font-medium text-[#190e61]">
               클릭해서 업로드 해주세요
             </p>

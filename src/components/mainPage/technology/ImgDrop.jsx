b823616f8925369d088d7d179cd2e94a4efa4a7b
SIM-TOO <lsh20806172@gmail.com>
2023-10-23 10:56:12 +0900

    1023_10:55_이승훈_이미지업로드 페이지 화면 구현 완료

--- src/components/mainPage/technology/ImgDrop.jsx
+++ src/components/mainPage/technology/ImgDrop.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
-import { useCallback } from 'react';
 import axios from 'axios';
 
-function ImgDrop() {
+function ImgDrop({ onUploadSuccess }) {
+  const [imageSrc, setImageSrc] = useState(null);  // 이미지의 URL 또는 base64 데이터를 저장할 상태 변수
+
   const onDrop = useCallback((acceptedFiles) => {
     const url = process.env.REACT_APP_MASTER_URL;
     console.log("이미지 업로드 시도");
-   
+
+    // 이미지 미리보기 설정
+    const file = acceptedFiles[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      setImageSrc(event.target.result);  // 이미지 데이터를 상태 변수에 저장
+    };
+
+    reader.readAsDataURL(file);
 
     // 이미지 서버로 업로드
     const formData = new FormData();
@@ -34,15 +44,28 @@ function ImgDrop() {
   });
 
   return (
-    <div {...getRootProps()} className="col-span-12 border-2 border-gray-300 rounded-md hover:border-blue-500 cursor-pointer h-[100%] flex items-center justify-center">
+    <div {...getRootProps()} className="col-span-12 p-3 border-2 border-gray-300 rounded-md hover:border-blue-500 cursor-pointer h-[100%] flex items-center justify-center">
       <input {...getInputProps()} />
-      <p className="opacity-50 font-medium text-[#190e61]">
-        이미지를 드래그를 통해
-        <br />
-        이곳에 위치시키세요
-      </p>
+      {
+        imageSrc ?
+          <img src={imageSrc} alt="Uploaded Preview" className="object-cover" />
+          :
+          <>
+            <p className="hidden md:block opacity-50 font-medium text-[#190e61]">
+              이미지를 드래그를 통해
+              <br />
+              이곳에 위치시키세요
+            </p>
+
+            <p className="block md:hidden opacity-50 font-medium text-[#190e61]">
+              클릭해서 업로드 해주세요
+            </p>
+          </>
+      }
     </div>
   );
 }
 
 export default ImgDrop;
+
+

426f4ebfb225fcedb574486f949fd8a9940e88c5
philosokey-M <wlghd99589@gmail.com>
2023-10-25 12:03:12 +0900

    1025_12:03_김지홍코인차감 최종

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -31,8 +31,7 @@ function Join() {
             <img src="https://i.ibb.co/BCP0xW5/join.png" className="absolute top-0 left-0 z-10 hidden md:block" />
             <UserImage />
           </div>
-          <div class="mx-auto p-0 min-w-[300px] lg:mt-0 mt-60 lg:min-w-[600px]">
-
+          <div className="mx-auto lg:p-10 lg:min-w-[600px] p-0 min-w-[300px]">
             <p className="text-3xl font-medium text-left text-black p-3">Sign in</p>
 
             {/* from 태그 시작 */}

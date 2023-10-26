39cf7b1426519867f7932497bf9fc8c146d7c18e
philosokey-M <wlghd99589@gmail.com>
2023-10-26 10:40:41 +0900

    1026_10:40_김지홍_env에 flask 주소 추가

--- src/hook/mainPage/useGetCoin.js
+++ src/hook/mainPage/useGetCoin.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+import { TokenRefresherContext } from '../../context/TokenRefresherContext';
+import { useContext } from "react";
+
 
 const useGetCoin = () => {
     const url = process.env.REACT_APP_MASTER_URL;
     axios.defaults.withCredentials = true;
+    const TokenRefresher = useContext(TokenRefresherContext);
     const getCoin = async (email) => {
         try {
-           const response = await axios.post(`${url}/coin/findCoin`); 
+           const response = await TokenRefresher.post(`${url}/coin/findCoin`); 
             if (response && response.data) {
                 console.log(response);
                 return response.data;

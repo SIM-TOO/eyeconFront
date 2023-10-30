ae13c8baef9a05cb4327a7ad43c139d5433a0b94
vlfgus <vlfgus321@gmail.com>
2023-10-30 17:08:30 +0900

    1030_1708_문필현_커밐키키키키킼

--- src/components/resultList/ListSet.jsx
+++ src/components/resultList/ListSet.jsx
@@ -6,14 +6,8 @@ const List = () => {
   return (
 
     <div className="container font-Pretendard mx-auto grid grid-cols-12 max-w-screen-x">
-      <div className='col-span-12 grid grid-cols-12 gap-4 flex items-center'>
-        <div className='col-span-4' />
-
-
-        <div className='col-span-3' />
-      </div>
-
-      <div className='col-span-1 md:col-span-2' />
+      
+      <div className='col-span-4 md:col-span-2' />
       {/* 이거, 모바일화면에서는 col-span-10이 맞는데, pc화면에서는 조금 줄일 거 고려해야할듯 */}
       <div className='bg-white col-span-10 md:col-span-8 flex flex-col justify-center items-center rounded-[25px] border' style={{
         width: "100%", height: "100%", padding: "20px"
@@ -29,18 +23,17 @@ const List = () => {
             <div className='col-span-4' />
           </div>
 
-          <div className="col-span-12 grid gird-cols-12  md:col-span-4 group rounded-lg border border-[#d9d9d9] p-5  bg-[#FFFFFF] bg-opacity-10">
-            <p className='col-span-2 grid gird-cols-2 text-2xl font-bold text-left text-black p-2'> 2023-10-30 </p>
-            <svg className='col-span-1'>
+          <div className="col-span- grid gird-cols-36  md:col-span-4 group rounded-lg border border-[#d9d9d9] p-5  bg-[#FFFFFF] bg-opacity-10">
+            <p className='text-2xl font-bold text-left text-black p-2'> 2023-10-30 </p>
+            <svg className='col-span-3'>
               <line y1="0.5" x2={486} y2="0.5" stroke="#CACACA" />
             </svg>
-            <div className='col-span-4' />
             <div className='bg-white rounded-[20px]' style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}>
               <img src="https://i.ibb.co/6rWHYNc/1213.png" alt="1213" border="0" />
-              <div className='col-span-4 md:col-span-4'>
-                <div className='col-span-4 grid-cols-4'>
-                  <p className='col-span-2 text-2xl font-bold text-left p-2'> 시선분석 전 </p>
-                  <button className='col-span-2 md:col-span-4 border-[1.5px] border-[#19ab93] rounded-[20px] w-[116px] h-auto text-center'>
+              <div className='col-span-6 md:col-span-4'>
+                <div className='col-span-6 grid-cols-4'>
+                  <p className='col-span-6 text-2xl font-bold text-left p-2'> 시선분석 전 </p>
+                  <button className='col-span-6 md:col-span-4 border-[1.5px] border-[#19ab93] rounded-[20px] w-[116px] h-auto text-center'>
                     Download ↓
                   </button>
                 </div>

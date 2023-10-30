b51492e29c7a2c76929ab485b45089f070bc8b44
chgo0506 <chgo0506@gmail.com>
2023-10-31 01:08:12 +0900

    1031_01:08_류상지_결제 화면 수정

--- src/components/mainPage/C02Pay.jsx
+++ src/components/mainPage/C02Pay.jsx
@@ -12,20 +12,20 @@ const C02Pay = ({ handleButtonClick }) => {
   // removeCoin 함수를 가져옵니다.
   const removeCoin = useRemoveCoin();
 
-  
+
   // 코인 차감 함수
   const modifiedHandleButtonClick = async (value) => {
 
     // 쿠키 (토큰 -access ->xxxx.xxxx.x(email) )
     const email = "wlghd9958@naver.com";
 
-    if (coinValue > 0){
+    if (coinValue > 0) {
       // 현재 코인 반환
-    const result = await removeCoin(email);
-    // console.log(result)
-    console.log("차감완료")
-   
-    handleButtonClick(value);
+      const result = await removeCoin(email);
+      // console.log(result)
+      console.log("차감완료")
+
+      handleButtonClick(value);
     } else {
       console.log("코인 부족")
       Swal.fire({
@@ -33,7 +33,7 @@ const C02Pay = ({ handleButtonClick }) => {
         title: '코인 부족',
         text: '코인이 부족합니다. 충전 후 이용해주세요.',
       });
-      
+
     }
   };
 
@@ -45,24 +45,26 @@ const C02Pay = ({ handleButtonClick }) => {
       <div className="hidden md:block col-span-0 md:col-span-1" />
 
       {/* 잔여 코인 확인 박스 */}
-      <div className="col-span-12 h-full md:col-span-5 grid grid-cols-12 p-3 bg-white rounded shadow border border-[#DEE2E7]">
-        <p className="col-span-12 text-2xl font-bold text-left text-[#505050]  p-3">
+      <div className="rounded-[10px] col-span-12 h-full md:col-span-5 grid grid-cols-12 p-3 bg-white rounded shadow border border-[#DEE2E7]">
+
+        <p className="col-span-12 text-2xl font-bold text-left text-[#505050]  p-3 text-center">
+        <img src="https://i.ibb.co/CbxhQmw/icon-check-circle-1.png" className="w-8 inline-block mr-2" />
           잔여코인 확인
         </p>
 
         <div className="h-[150px] col-span-12 grid grid-cols-12">
-          <p className="col-span-6 text-left p-3">현재 코인 :</p>
-          <p className="col-span-6 text-right p-3"> {coinValue} 개</p>
-          <p className="col-span-6 text-left p-3">필요 코인 :</p>
-          <p className="col-span-6 text-right p-3 text-[#fa3434]"> - 1 개</p>
+          <p className="col-span-6 text-left p-3 text-xl"> 현재 코인 :</p>
+          <p className="col-span-6 text-right p-3 text-lg "> <img src="https://i.ibb.co/Db5RC1S/emoji-coin.png" className="w-[25px] mr-2 inline-block" alt="coin" /> {coinValue} 개</p>
+          <p className="col-span-6 text-left p-3 text-xl  font-bold ">필요 코인 :</p>
+          <p className="col-span-6 text-right p-3 text-[#fa3434] text-lg  font-bold "> - 1 개</p>
         </div>
         {/* 줄 */}
         <div className="col-span-12 h-[2px] bg-[#e4e4e4] " />
 
         {/* 내용 */}
         <p className="col-span-12 p-3" />
-        <p className="col-span-12 text-left p-3">
-          시선분석 서비스를 이용하기 위해서는 코인이 1개 필요합니다. 코인사용
+        <p className="col-span-12 text-left p-3 text-center text-lg">
+          시선분석 서비스를 이용하기 위해서는 코인이 1개 필요합니다. <br/>코인사용
           버튼을 누르면 시선 분석이 시작됩니다
         </p>
         <p className="col-span-12 p-3" />
@@ -86,8 +88,8 @@ const C02Pay = ({ handleButtonClick }) => {
       <div className="hidden md:block col-span-0 md:col-span-1" />
 
       {/* 이미지 */}
-      <div className="font-Pretendard h-full col-span-12 grid grid-cols-12  md:col-span-4 p-3 bg-white rounded shadow border border-[#DEE2E7]">
-        <p className="col-span-12 text-2xl font-bold text-left text-[#505050]  p-3">
+      <div className="rounded-[10px] font-Pretendard h-full col-span-12 grid grid-cols-12  md:col-span-4 p-3 bg-white rounded shadow border border-[#DEE2E7]">
+        <p className="col-span-12 text-2xl font-bold text-[#505050] text-center p-3 ">
           추가 서비스 이용 방법
         </p>
         <img
@@ -100,14 +102,15 @@ const C02Pay = ({ handleButtonClick }) => {
 
         {/* 내용 */}
         <p className="col-span-12 p-3" />
-        <p className="col-span-12 text-left p-3">
-          잔여 코인이 부족하거나 추가적인 서비스 결제가 필요하세요?
+        <p className="col-span-12 text-center p-3 text-lg">
+          잔여 코인이 부족하거나 <br/>
+          추가적인 서비스 결제가 필요하세요?
         </p>
         <p className="col-span-12 p-3" />
 
         {/* 버튼 */}
         <p className="col-span-3" />
-        <button className="h-[60px] col-span-6 justify-center items-center rounded-lg bg-[#40c3ae]">
+        <button className="h-[60px] col-span-6 justify-center items-center rounded-lg bg-gray-500">
           <Link
             to="/mainpay"
             className="text-lg font-semibold text-center text-white"

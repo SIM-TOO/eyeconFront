03a0658c5314142a4f4acbef4bf4db3e79205ff1
chgo0506 <chgo0506@gmail.com>
2023-10-31 00:38:44 +0900

    10:31_12:38_류상지_3초 뒤 컴포넌트 메인 이동

--- src/components/PayPage/CoinCheck.jsx
+++ src/components/PayPage/CoinCheck.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Lottie from 'react-lottie-player'
 import Header from "../HeaderAfter";
 import lottieJson from '../../lottie/coineffect.json';
 import lottieJson2 from '../../lottie/progress.json';
 import useGetCoin from "../../hook/mainPage/useGetCoin";
-import { useLocation } from 'react-router-dom';
+import { useLocation, Navigate, useNavigate } from 'react-router-dom';
 
 
 
@@ -18,7 +18,10 @@ const CoinCheck = () => {
 
     const remainingCoins = coins
 
+
     const location = useLocation();
+    const navigate = useNavigate();
+
     const coinmoney = location.state?.coinCount || 0; // 기본값 0으로 설정
     const coinCount = location.state?.coinCnt || 0; // 기본값 0으로 설정
 
@@ -27,9 +30,20 @@ const CoinCheck = () => {
     const fetchCoinInfo = async () => {
         await GetCoin("");
     };
-    fetchCoinInfo();
 
+    useEffect(() => {
+        fetchCoinInfo();
 
+        // 3초 후에 main 페이지로 이동
+        const timeoutId = setTimeout(() => {
+            navigate('/main');
+        }, 3000);
+
+        // 컴포넌트가 언마운트 될 때 타임아웃 클리어
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [navigate]);
 
     return (
         <div>
@@ -46,7 +60,7 @@ const CoinCheck = () => {
                 />
 
 
-                
+
                 <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                     <h5 className="mb-2 text-4xl font-bold text-gray-900 dark:text-white">코인 {coinCount}개 충전 완료</h5>
                     <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">코인이 충전되었습니다. 잠시 후 메인 화면으로 이동합니다.</p>
@@ -58,13 +72,13 @@ const CoinCheck = () => {
                                 <div className="-mt-1 font-sans text-sm font-semibold text-center">{remainingCoins}개</div>
                             </div>
                         </div>
-                        
+
                         <div className="w-full sm:w-auto bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-8 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                             <div className="text-left">
                                 <div className="mb-1 text-sm">충전 금액</div>
                                 <div className="-mt-1 font-sans text-sm font-semibold text-center">{coinmoney}원</div>
                             </div>
-                            
+
                         </div>
                     </div>
                     <div className="flex items-center justify-center">
@@ -76,12 +90,12 @@ const CoinCheck = () => {
                             style={{ width: "100%", maxWidth: "400px" }}
                             className="mb-10"
                         />
-                        </div>
                     </div>
                 </div>
-
-
             </div>
+
+
+        </div>
 
     );
 };

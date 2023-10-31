c685577808bbba990f400b6e54bd1b908be92b5b
SIM-TOO <lsh20806172@gmail.com>
2023-10-31 12:17:19 +0900

    1031_12:17_이승훈_코드 가독성 패치중

--- src/hook/userPage/useUserAddress.js
+++ src/hook/userPage/useUserAddress.js
@@ -2,13 +2,10 @@ import { useState, useEffect } from 'react';
 
 const useUserAddress = () => {
     const [address, setAddress] = useState('');
-    const [dong, setDong] = useState('');
     useEffect(() => {
         const savedAddress = localStorage.getItem('address');
-        const savedAddress2 = localStorage.getItem('dong')
         if (savedAddress) {
             setAddress(savedAddress);
-            setDong(savedAddress2);
         }
     }, []);
 
@@ -29,7 +26,6 @@ const useUserAddress = () => {
     const handleAddressChange = (newAddress) => {
         setAddress(newAddress);
         localStorage.setItem('address', newAddress);
-        // localStorage.setItem('dong', );
     };
 
     return {

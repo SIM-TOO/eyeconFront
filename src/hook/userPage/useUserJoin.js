9168da31d2543a11d7d6cdbd5adc2871b590cbf2
philosokey-M <wlghd99589@gmail.com>
2023-10-31 09:53:41 +0900

    1031_09:53_김지홍_주소 로컬 스토리지 버그 수정

--- src/hook/userPage/useUserJoin.js
+++ src/hook/userPage/useUserJoin.js
@@ -42,9 +42,11 @@ const useUserJoin = (IdErrorMessage, PWErrorMessage, PWMErrorMessage) => {
     // POST 요청 방식
     try {
       axios.post(`${url}/user/join`, dataToSend);
+      localStorage.clear();
       window.location.href = '/login'
     } catch (error) {
       console.error("전송을 실패 했습니다 에러 내용 :", error);
+      localStorage.clear();
     }
   };
 

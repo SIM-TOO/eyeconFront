9e061a88fdb76357079422d191fa427b091244df
chgo0506 <chgo0506@gmail.com>
2023-11-01 11:35:01 +0900

    1101_11:32_류상지_hook주석 추가, 코인결제 페이지 수정

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -6,12 +6,9 @@ import MyPage from './myPage/MyFixTest'
 
 function Mypage() {
   return (
-    <div className="h-screen bg-no-repeat bg-cover md:bg-[url('https://i.ibb.co/Q8M7ycH/Kakao-Talk-20231018-121457122.png')] dark:bg-[url('https://i.ibb.co/PD92ZQZ/1.png')] ">
-
-
+    <div className="h-screen bg-no-repeat bg-cover md:bg-[url('https://i.ibb.co/Q8M7ycH/Kakao-Talk-20231018-121457122.png')] dark:bg-[url('https://i.ibb.co/PD92ZQZ/1.png')]">
       <Header />
       <MyPage />
-     
     </div>
   );
 }

61dea9541aab59bff8d1f7a13e23e08a5ddf4763
SIM-TOO <lsh20806172@gmail.com>
2023-11-01 12:06:31 +0900

    1031_12:06_이승훈_결제내역 페이지 및 기능 완료

---
+++ src/components/PayPage/PayHistory.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const PayHistory = ({ paymentHistory }) => {
+    return (
+        <div className="font-Pretendard text-center container grid grid-cols-12 p-3 gap-4">
+
+            {/* 여백용 박스 */}
+            <div className="col-span-12 p-3" />
+
+            {/* 타이틀 */}
+            <h2 className="text-2xl font-semibold mb-4 col-span-12">결제 내역</h2>
+
+            {/* 여백용 박스 */}
+            <div className='col-span-1' />
+            {/* 내역박스 */}
+            <div className='col-span-10 grid grid-cols-12 gap-3'>
+                <hr className="col-span-12 my-4" />
+                <div className="col-span-6 md:col-span-2">날짜</div>
+                <div className="col-span-6 md:col-span-2">시간</div>
+                <div className="col-span-12 md:col-span-2">상품명</div>
+                <div className="col-span-4 md:col-span-2">코인 개수</div>
+                <div className="col-span-4 md:col-span-2">금액</div>
+                <div className="col-span-4 md:col-span-2">결제 방식</div>
+                <hr className="col-span-12 my-4" />
+
+                {/* Map함수 */}
+                {paymentHistory.map((payment, index) => (
+                    <>
+                        <div key={index} className="text-gray-500 col-span-6 md:col-span-2">{payment.date}</div>
+                        <div className="text-gray-500 col-span-6 md:col-span-2">{payment.time}</div>
+                        <div className="text-gray-600 col-span-12 md:col-span-2">{payment.coin_id === 0 ? "BASIC" : payment.coin_id === 1 ? "PREMIUM" : payment.coin_id === 2 ? "PRO" : ""}</div>
+                        <div className="text-gray-600 col-span-4 md:col-span-2">{payment.coin}</div>
+                        <div className="text-gray-600 col-span-4 md:col-span-2">{payment.price}</div>
+                        <div className="text-gray-600 col-span-4 md:col-span-2">{payment.finance}</div>
+                        <hr className="col-span-12 my-4" />
+                    </>
+                ))}
+            </div>
+
+            {/* 여백용 박스 */}
+            <div className='col-span-1' />
+        </div >
+    )
+}
+export default PayHistory

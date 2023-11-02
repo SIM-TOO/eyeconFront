80fa68fb5f91b44df3b35c73d7dd9b8b9af4ab14
SIM-TOO <lsh20806172@gmail.com>
2023-11-02 16:19:07 +0900

    1102_16:16_이승훈_결과페이지 2차 커밋용

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import HeaderAfter from './HeaderAfter';
 import ListSet from './resultList/ListSet'
 import ResultMain from './resultList/ResultMain';
@@ -7,10 +6,7 @@ import ResultMain from './resultList/ResultMain';
 function ResultPage() {
 
   return (
-
     <div className="h-screen bg-no-repeat bg-cover md:bg-[url('https://i.ibb.co/Q8M7ycH/Kakao-Talk-20231018-121457122.png')] dark:bg-[url('https://i.ibb.co/PD92ZQZ/1.png')]">
-
-
       {/* 헤더 */}
       <HeaderAfter />
 
--- src/components/resultList/ListSet.jsx
+++ src/components/resultList/ListSet.jsx
@@ -89,48 +89,6 @@ function DateAndData({ date, data }) {
 
 const List = () => {
   return (
-<<<<<<< HEAD
-    <div className="flex items-center justify-center">
-      <div className="w-[95%] md:w-[70%] rounded-2xl p-4">
-        <div className="container mx-auto grid grid-cols-12 gap-4 max-w-screen-xl h-full p-4">
-          <div className='col-span-12 grid grid-cols-12 rounded-[10px] bg-white rounded shadow border border-[#DEE2E7] p-3'>
-            <div className='col-span-12 grid grid-cols-12 gap-2 p-4'>
-              <div className='col-span-4 p-4' />
-              <p className='col-span-4 font-bold text-2xl text-center p-4'>시선 분석 결과</p>
-              <div className='col-span-4 p-4' />
-            </div>
-            <div className='col-span-12 grid grid-cols-12 gap-4 p-1'>
-              <div className='col-span-3 flex items-center p-1'>
-                <button className="rounded-[15px] bg-[#46cfb9] border border-[#46cfb9] p-1" style={{ boxShadow: "0px 2px 4px 0 rgba(0,0,0,0.43)", color: "white" }}>
-                  내 가게 선택
-                </button>
-              </div>
-              <div className='col-span-9 flex items-center justify-end p-1'>
-                <p className='text-right p-2'>↓ 최신순</p>
-              </div>
-            </div>
-            <div className='col-span-12 grid grid-cols-12 rounded-[10px] bg-white rounded shadow border border-[#DEE2E7] p-3'>
-              <div className='col-span-12 grid grid-cols-12 gap-4 p-2'>
-                <p className='col-span-4 p-2 text-bold text-2xl'>2023-11-01</p>
-                <div className='col-span-4 p-2' />
-                <div className='col-span-4 p-2' />
-              </div>
-              <div className="rounded-[10px] col-span-12 h-full md:col-span-5 grid grid-cols-12 p-3 bg-white rounded shadow border border-[#DEE2E7]">
-                <img src="https://i.ibb.co/6Y1kC07/image.jpg" alt="image" border="0" />
-              </div>
-              <div className=''/>
-              <div className="rounded-[10px] col-span-12 h-full md:col-span-5 grid grid-cols-12 p-3 bg-white rounded shadow border border-[#DEE2E7]">
-                <img src="https://i.ibb.co/61WLv05/image.jpg" alt="image" border="0" />
-              </div>
-              <div className='col-span-12'>
-                <p>
-                  광주 동구 상업단지 상권 40대 남성을 타겟으로 한 매대 시선분석 결과입니다.
-                  현재 광주 동구 충장로 상권의 경우 40대 남성을 주 대상으로 하고 있습니다.
-                  주요 연령대와 성별을 대상으로 한 매대 시선 분석 결과를 활용해 마케팅에 활용해보세요.
-                </p>
-              </div>
-            </div>
-=======
     <div className="flex items-center justify-center w-full">
       <div className="w-[100%] md:w-[70%] rounded-2xl dark:bg-gradient-[#0a0a0a] bg-gradient-[#fff]">
         <div className="container mx-auto grid grid-cols-12 md:col-span-8 gap-4 max-w-screen-xl h-full p-1">
@@ -140,7 +98,6 @@ const List = () => {
             {dates.map((date, index) => (
               <DateAndData key={index} date={date} data={data} />
             ))}
->>>>>>> 5ba8fbdd2470b1dd720e7d61a7296e091f63cb26
           </div>
         </div>
       </div>

f313ae3944dce5631f44ddbd83b8aaa9f23e450b
SIM-TOO <lsh20806172@gmail.com>
2023-11-03 12:11:54 +0900

    1103_12:11_이승훈_AI채팅창 버그 수정

--- src/hook/mainPage/useChat.js
+++ src/hook/mainPage/useChat.js
@@ -6,7 +6,6 @@ const useChat = (initialMessage) => {
   const [waitingForResponse, setWaitingForResponse] = useState(false);
   const [messages, setMessages] = useState([]);
   const [initialized, setInitialized] = useState(false);
-  const loadingMessageId = 'loadingMessage'; // 로딩 중 메시지의 고유 식별자
 
   useEffect(() => {
     if (!initialized) {
@@ -22,26 +21,35 @@ const useChat = (initialMessage) => {
     const newMessage = { content: inputValue, isMine: true };
     setMessages((prevMessages) => [...prevMessages, newMessage]);
     setInputValue('');
+    console.log(inputValue);
 
     // 로딩 중 표시
     const loadingMessage = {
       content: '로딩중...',
       isMine: false,
-      id: loadingMessageId, // 로딩 중 메시지의 고유 식별자 추가
     };
     setMessages((prevMessages) => [...prevMessages, loadingMessage]);
     setWaitingForResponse(true);
     axios.defaults.withCredentials = false;
     try {
       // 서버로 메시지를 보냅니다.
+      // const urlf = process.env.REACT_APP_MASTER_URL1;
       const response = await axios.post(`http://localhost:5000/consult`, {
-        role: 'user',
+        'role': 'user',
         content: inputValue, // 메시지 내용을 서버로 보냅니다.
       });
       console.log(response);
 
       // 서버 응답 데이터를 가져옵니다.
+
       const serverResponse = response.data.choices?.[0]?.message;
+
+      // 테스트용
+      // const serverResponse = {
+      //     content: "대답중",
+      // };
+
+      console.log(serverResponse);
 
       const serverMessage = {
         content: serverResponse?.content,
@@ -49,17 +57,16 @@ const useChat = (initialMessage) => {
       };
 
       // 서버 응답을 받으면 "로딩 중..." 메시지를 삭제
-      setMessages((prevMessages) =>
-        prevMessages.filter((message) => message.id !== loadingMessageId)
-      );
-      setMessages((prevMessages) => [...prevMessages, serverMessage]);
+      setMessages((prevMessages) => prevMessages.filter((message) => message !== loadingMessage));
       setWaitingForResponse(false);
+
+      // 서버 응답
+      setMessages((prevMessages) => [...prevMessages, serverMessage]);
+
     } catch (error) {
       console.error('메시지 전송 중 오류 발생:', error);
       // 오류가 발생하면 "로딩 중..." 메시지를 삭제
-      setMessages((prevMessages) =>
-        prevMessages.filter((message) => message.id !== loadingMessageId)
-      );
+      setMessages((prevMessages) => prevMessages.filter((message) => message !== loadingMessage));
       setWaitingForResponse(false);
     }
 
@@ -74,6 +81,6 @@ const useChat = (initialMessage) => {
     messages,
     chatHandleSubmit,
   };
-};
+}
 
-export default useChat;
+export default useChat

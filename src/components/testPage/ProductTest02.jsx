2b1884d423734e083244412496fadb22e31bef9c
philosokey-M <wlghd99589@gmail.com>
2023-11-06 12:19:58 +0900

    1106_1219_김지홍-캡처포기

--- src/components/testPage/ProductTest02.jsx
+++ src/components/testPage/ProductTest02.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { fabric } from 'fabric';
 import axios from 'axios';
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
 
 const ProductTest02 = ({ beforeimg, hitmap}) => {
 
@@ -9,7 +11,7 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
     useEffect(() => {
         axios.post('http://localhost:5000/slice',{ 
             // beforeImgUrl은 result페이지에서 받아와야 됨
-           beforeImgUrl : "https://firebasestorage.googleapis.com/v0/b/eyecon-9b097.appspot.com/o/images%2F1699067778201?alt=media&token=ae991c1a-acf7-4ba9-8963-ba4d616ccb5f"})
+           beforeImgUrl : beforeimg})
         .then(response => { 
         const imageStrings = response.data;
         const urls = imageStrings.map(imageString => `data:image/jpeg;base64,${imageString}`);
@@ -17,42 +19,52 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
         });
     }, []);    
 
-    
-    // const handleCapture = () => {
-    //     // 캔버스 캡처 및 데이터 URL로 변환
-    //     const dataURL = canvasRef.current.toDataURL();
-    
-    //     // 캡처된 이미지를 새 창에서 열기
-    //     window.open(dataURL);
-    //   };
+    const canvasRef = useRef(null);
+
+    React.useEffect(() => {
+      canvasRef.current = document.getElementById('canvas');
+    }, []);
   
-    // 배경설정
-//     const setBg = () =>{
-//     fabric.Image.fromURL('https://media.istockphoto.com/id/1269284290/ko/%EC%82%AC%EC%A7%84/%EB%B9%88-%EC%83%81%EC%A0%90-%EC%84%A0%EB%B0%98%EC%9D%B4%EC%9E%88%EB%8A%94-%EC%8A%88%ED%8D%BC%EB%A7%88%EC%BC%93-%EC%9D%B8%ED%85%8C%EB%A6%AC%EC%96%B4%EA%B0%80-%EC%A1%B0%EB%A1%B1%ED%95%A9%EB%8B%88%EB%8B%A4.jpg?s=2048x2048&w=is&k=20&c=nJGiNi42ZvU3MEtT-yzwDBJx2D-_Xam-mjlYMk1rvbM=', function (img) {
-//         // 이미지가 로드되면 캔버스의 배경으로 설정
-//         canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
-//             // 필요한 경우 옵션을 설정
-//             scaleX: canvas.width / img.width,
-//             scaleY: canvas.height / img.height
-//         });
-//     });   
-//     var rect = new fabric.Rect({
-//         left: canvas.width*0,
-//         top: canvas.height*0,
-//         fill: 'rgba(255,255,255,0.5)',
-//         width: 400,
-//         height: canvas.height,
-//         selectable: false 
-//       });
-//       canvas.add(rect);
-//     //   canvas.renderAll();
-// }
+    // const handleCapture = async () => {
+    //   // 캔버스 요소 선택
+    //   var canvas = document.getElementById('canvas');
+    
+    //   // 2D 렌더링 컨텍스트 가져오기
+    //   var ctx = canvas.getContext('2d');
+    
+    //   // 이미지 로드
+    //   var img = new Image();
+    //   img.crossOrigin = "anonymous";  // crossOrigin 속성 설정
+    //   img.src = "image_url";  // 이미지 URL
+    //   await new Promise((resolve) => {
+    //     img.onload = function() {
+    //       // 이미지를 캔버스에 그리기
+    //       ctx.drawImage(img, 0, 0);
+    //       resolve();
+    //     };
+    //   });
+    
+    //   // 캔버스의 전체 영역에 대한 픽셀 데이터 가져오기
+    //   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    
+    //   // 캔버스 데이터를 데이터 URL로 변환
+    //   var dataURL = canvas.toDataURL('image/png');
+    
+    //   // 데이터 URL을 Blob 객체로 변환
+    //   fetch(dataURL)
+    //     .then(res => res.blob())
+    //     .then(blob => {
+    //       // Blob 객체를 이미지 파일로 저장
+    //       saveAs(blob, 'canvas.png');
+    //     });
+    // };
+    
 
     // 상품 이미지 생성될 공간
     const eventory = () =>{
         fabric.Image.fromURL('https://firebasestorage.googleapis.com/v0/b/eyecon-9b097.appspot.com/o/planogram%2F%EA%B2%A9%EC%9E%90.png?alt=media&token=9fff1474-9297-495c-88e0-dfb9182f71bb', function(img) {
             img.set({
-              left: 0,
+              left: 100,
               top: 0,
               angle: 0,
               padding: 10,
@@ -73,8 +85,8 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
     const showHitmap = () =>{
         fabric.Image.fromURL(`${hitmap}`, function(img) {
             img.set({
-              left: 600,
-              top: 0,
+              left: 1300,
+              top: 100,
               angle: 0,
               padding: 10,
               cornersize: 10,
@@ -85,7 +97,7 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
             });
       
             img.scaleToWidth(400);
-            img.scaleToHeight(canvas.height);
+            img.scaleToHeight(canvas.height*0.6);
             // canvas.add(img).setActiveObject(img);
             canvas.add(img);
             canvas.sendToBack(img); // 이미지를 캔버스의 가장 아래 레이어로 보냄
@@ -94,7 +106,7 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
     const showCase = ()=>{
         fabric.Image.fromURL('https://firebasestorage.googleapis.com/v0/b/eyecon-9b097.appspot.com/o/planogram%2FKakaoTalk_20231104_144127727.png?alt=media&token=af772986-fde3-4dee-b409-af24700a0dcf', function(img) {
             img.set({
-              left: 900,
+              left: 600,
               top: 0,
               angle: 0,
               padding: 10,
@@ -122,7 +134,7 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
           fabric.Image.fromURL(url, function(img) {
             // 필요한 경우 옵션을 설정
             img.set({
-              left: 20 + col * 60, // 이미지의 가로 위치를 조정
+              left: 120 + col * 60, // 이미지의 가로 위치를 조정
               top: 40+row * 99, // 이미지의 세로 위치를 조정
               angle: 0,
               padding: 10,
@@ -155,27 +167,26 @@ const ProductTest02 = ({ beforeimg, hitmap}) => {
       };
 
     useEffect(() => {
-        if (imageUrls.length > 0) {  // imageUrls 상태가 업데이트되었는지 확인합니다.
-            // setBg();
+        if (canvasRef.current && imageUrls.length > 0) {
+            const canvas = new fabric.Canvas(canvasRef.current, {
+              width: 1920,
+              height: 880,
+            });
             eventory();
             showCase();
             createProduct();
+            showHitmap();
         }
-    }, [imageUrls]);  // 의존성 배열에 imageUrls를 추가합니다
+    }, [canvasRef,imageUrls]);  // 의존성 배열에 imageUrls를 추가합니다
 
     useEffect( () => {        
-        
-        
-        
         canvas.renderAll();
-        // canvas.renderAll();
-
     },[]);
 
 
     return (
         <div>
-            <div>ProductTest02</div>
+            {/* <button className="rounded-3xl border-[2px] border-[#19ab93] p-2 text-sm font-bold" onClick={handleCapture} > capture </button> */}
             <div>
                 <canvas id='canvas' width='1920'  height='880'></canvas>
             </div>
